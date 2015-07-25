62770ed039e067fc03ad3f3473f7f9236bc48e56
Thomas Logan <tdloganj@gmail.com>
2015-07-25 16:13:15 -0700

    [pokedex controller] firebase has been hooked up properly.

--- client/components/pokedex/pokedex.controller.js
+++ client/components/pokedex/pokedex.controller.js
@@ -1,3 +1,6 @@
-RidemonApp.controller('PokedexController', ['$scope', function($scope) {
+RidemonApp.controller('PokedexController', ['$scope', '$firebase', '$firebaseObject', function($scope, $firebaseObject) {
   $scope.welcome = "Welcome to the Pokedex!!!"
+
+  var pokeData = new Firebase("https://ridemon.firebaseio.com/");
+  console.log(pokeData);
 }]);

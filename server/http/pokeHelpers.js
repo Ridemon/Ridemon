61ef2f9fd4569d3e4074544258c5a7194f2cb90a
Tessa Kelly <tessakkelly@gmail.com>
2015-07-26 09:21:20 -0700

    refactor http requests from frontend to be on the server side as part of pokeHelpers.js

--- server/http/pokeHelpers.js
+++ server/http/pokeHelpers.js
@@ -22,9 +22,40 @@ module.exports.addPokemon = function(req, response) {
   response.end()
 };
 
+var capitalize = function(word) {
+  if(!word) return "";
+  var firstLetter = word[0].toUpperCase();
+  var restWord = word.slice(1).toLowerCase();
+  return firstLetter + restWord;
+};
+
+var getOnePokemon = function(pokeId, callback) {
+  var pokey = {};
+  request({
+    url: "http://pokeapi.co/api/v1/pokemon/" + pokeId,
+    method: "GET"
+  }, function(error, res, data) {
+    data = JSON.parse(data);
+    pokey.abilities = data.abilities;
+    if(data.evolutions.length > 0){
+      pokey.evolvesTo = data.evolutions[0].to;
+    }
+    request({
+      url: "http://pokeapi.co/api/v1/sprite/" + pokeId,
+      method: "GET"
+    }, function(error, res, data) {
+      data = JSON.parse(data);
+      pokey.name = capitalize(data.pokemon.name);
+      pokey.image = data.image;
+      callback(pokey);
+    })
+  });
+};
+
+var pokemonArray = [];
+
 module.exports.getPokemon = function(req, response) {
   var userId = req.session.userId;
-  var pokemonArray = [];
 
   request({
     url: 'https://ridemon.firebaseio.com/users/userIds/' + userId + '.json',
@@ -35,7 +66,20 @@ module.exports.getPokemon = function(req, response) {
     }
     
     if(body) {
-      response.end(JSON.stringify(JSON.parse(body).pokemonIds));
+      var pokemonIds = JSON.parse(body).pokemonIds;
+      var index = 0, count = 0;
+      for(var pokemonId in pokemonIds) {
+        +(function(ind) {
+          getOnePokemon(pokemonId, function(data) {
+            pokemonArray[ind] = data;    
+            count++;
+            if(count === index) {
+              response.send(pokemonArray);
+            }
+          });
+        })(index);
+        index++;
+      }
     } else {
       response.end(JSON.stringify({pokemonIds: {}}));
     }

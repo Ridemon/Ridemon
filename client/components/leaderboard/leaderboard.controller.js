e37901a4aac44aead460679f49d6a1ac61ace885
Tessa Kelly <tessakkelly@gmail.com>
2015-07-26 14:46:04 -0700

    Make progress on leaderboard

--- client/components/leaderboard/leaderboard.controller.js
+++ client/components/leaderboard/leaderboard.controller.js
@@ -1,10 +1,28 @@
 RidemonApp.controller('LeaderboardController', ['$scope', '$firebase', '$firebaseArray', '$firebaseObject', "$http", function($scope, $firebase, $firebaseArray, $firebaseObject, $http) {
   var users = new Firebase('https://ridemon.firebaseio.com/users/userIds');
-  var userArray = $firebaseArray(users);
-  //grab user and pokemon data
-  var userPokemon = users.child('name');
-  $scope.userPokemonArray = $firebaseObject(userPokemon);
-  $scope.userName = $firebaseObject(users.child('name'));
-  $scope.userArray = userArray;
+  $scope.userArray = [];
+
+  users.orderByKey().on("child_added", function(snapshot) {
+    $scope.userArray.unshift(getAllPokemon(snapshot.val(), snapshot.key()));
+  });
+
+  function getAllPokemon(user, userId) {
+    $http.get("/pokedex", {params: {
+      specialUid: userId
+    }}).success(function(data) {
+      user.Uid = userId;
+      user.pokemon = data.map(function(info) {
+        return info.name;
+      }).join(", ");
+      user.count = 0;
+      for(var i in user.pokemonIds) {
+        user.count++;
+      }
+      })
+      .error(function(err) {
+        console.error(err);
+      });
+    return user;
+  };
 
 }])

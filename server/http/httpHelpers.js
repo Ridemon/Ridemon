9032e4aad8fae2182d6e6c05e1ea972368cdd3fd
travisjn <tdizzletrav@gmail.com>
2015-07-25 21:03:16 -0700

    Fix oauth to actually work

--- server/http/httpHelpers.js
+++ server/http/httpHelpers.js
@@ -1,5 +1,5 @@
 var request = require('request');
-
+var token = require('../../server');
 /*
 Ride Request
 
@@ -15,20 +15,55 @@ surge_confirmation_id (optional)  string  The unique identifier of the surge ses
 */
 
 module.exports.requestRide = function(req, res) {
-  request({
-    url: 'https://sandbox-api.uber.com/v1/requests',
-    method: 'POST',
-    qs: req.data,
-    headers: {
-      'Content-Type': 'application/JSON',
-    }
-  }, function(error, res, body) {
-    console.log('body:', body);
-    console.log('error:', error);
-    response.send(JSON.parse(body));
-  })
+
+  // First get uber products for the area of request
+  var latitude = 33,
+      longitude = 123;
+
+  getProducts(latitude, longitude, function(data) {
+
+    console.log(data);
+    res.end();
+  });
+/*
+    request({
+      url: 'https://sandbox-api.uber.com/v1/requests',
+      method: 'POST',
+      qs: {
+        product_id: 33,
+        start_latitude: latitude,
+        start_longitude: longitude,
+        end_latitude: 32,
+        end_longitude: 123
+      },
+      headers: {
+        'Content-Type': 'application/JSON',
+      }
+    }, function(error, res, body) {
+      console.log('body:', body);
+      console.log('error:', error);
+      response.send(JSON.parse(body));
+    })
+  });
+*/
 };
 
-
+var getProducts = function(lat, long, callback) {
+  console.log(token);
+  request({
+    url: 'https://api.uber.com/v1/products',
+    method: 'GET',
+    qs: {
+      latitude: lat,
+      longitude: long
+    },
+    headers: {
+      'Content-Type': 'application/JSON',
+      'Authorization': 'Bearer ' + token
+    }
+  }, function(error, res, body) {
+    callback(body);
+  });
+};
 
 

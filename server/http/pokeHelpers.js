1ef340a34dc7c0c560025bf9538f732ce5697ef9
Thomas Logan <tdloganj@gmail.com>
2015-07-25 22:23:18 -0700

    setting up PUT to firebase.

--- server/http/pokeHelpers.js
+++ server/http/pokeHelpers.js
@@ -4,10 +4,28 @@ module.exports.requestPokemon = function(req, response) {
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+  };
 
   var pokemonId = getRandomInt(1, 151);
   console.log(pokemonId);
+
+  var savePokemonId = function(req, response) {
+    request({
+      url: "https://ridemon.firebaseio.com/pokemonIds",
+      method: 'PUT',
+      qs: req.data,
+      headers: {
+        'Content-Type': 'application/JSON',
+      }
+    }, function(error, res, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("Hello, World");
+      } else {
+        console.error('error:', error);
+      }
+    })
+  };
+
 
   request({
     url: "http://pokeapi.co/api/v1/sprite/" + pokemonId + "/",
@@ -20,6 +38,7 @@ module.exports.requestPokemon = function(req, response) {
     if (!error && response.statusCode == 200) {
       console.log(JSON.parse(body)) 
       response.send("Hello, World");
+      savePokemonId(pokemonId, res);
     } else {
       console.log('error:', error);
     }

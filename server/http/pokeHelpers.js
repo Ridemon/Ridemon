b445caf3b6ed00157a1b5f99e9e3f997981fbd14
Thomas Logan <tdloganj@gmail.com>
2015-07-25 23:10:31 -0700

    posting id's to the database

--- server/http/pokeHelpers.js
+++ server/http/pokeHelpers.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var Firebase = require('firebase');
 
 module.exports.requestPokemon = function(req, response) {
 
@@ -7,42 +7,15 @@ module.exports.requestPokemon = function(req, response) {
   };
 
   var pokemonId = getRandomInt(1, 151);
-  console.log(pokemonId);
 
-  var savePokemonId = function(req, response) {
-    request({
-      url: "https://ridemon.firebaseio.com/pokemonIds",
-      method: 'PUT',
-      qs: req.data,
-      headers: {
-        'Content-Type': 'application/JSON',
-      }
-    }, function(error, res, body) {
-      if (!error && response.statusCode == 200) {
-        console.log("Hello, World");
-      } else {
-        console.error('error:', error);
-      }
-    })
+  var savePokemonId = function() {
+    var userId = 1;
+    var myFirebaseRef = new Firebase("https://ridemon.firebaseio.com/users/userIds/" + userId + "/pokemonIds/" + pokemonId + "/");
+    myFirebaseRef.set({
+      caught: true
+    });
   };
 
-
-  request({
-    url: "http://pokeapi.co/api/v1/sprite/" + pokemonId + "/",
-    method: 'GET',
-    qs: req.query,
-    headers: {
-      'Content-Type': 'application/JSON',
-    }
-  }, function(error, res, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body)) 
-      response.send("Hello, World");
-      savePokemonId(pokemonId, res);
-    } else {
-      console.log('error:', error);
-    }
-    response.end();
-  })
+  savePokemonId();
 };
 

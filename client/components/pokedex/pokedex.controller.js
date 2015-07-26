0ae31fbcf5e61ff5bdcec6c463688f080573f863
Tessa Kelly <tessakkelly@gmail.com>
2015-07-25 23:07:31 -0700

    add getPokemon function for getting pokemon info and image according to pokemon id from pokeapi. Uses stubbed out list of ids

--- client/components/pokedex/pokedex.controller.js
+++ client/components/pokedex/pokedex.controller.js
@@ -1,8 +1,8 @@
-RidemonApp.controller('PokedexController', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
+RidemonApp.controller('PokedexController', ['$scope', '$firebaseObject', '$http', function($scope, $firebaseObject, $http) {
   var pokeData = new Firebase("https://ridemon.firebaseio.com/");
   var testObj = $firebaseObject(pokeData);
 
-  $scope.capitalize = function(word) {
+  var capitalize = function(word) {
     if(!word) return "";
     var firstLetter = word[0].toUpperCase();
     var restWord = word.slice(1).toLowerCase();
@@ -14,8 +14,32 @@ RidemonApp.controller('PokedexController', ['$scope', '$firebaseObject', functio
     window.speechSynthesis.speak(msg);
   };
 
-  $scope.pokemon
-  // TODO: get pokemon. Looking for an array of pokemon objects, 
+  $scope.pokemon = [];
+  // TODO: get actual pokemon ids. Looking for an array of pokemon objects, 
   // with name, captured_date, description, and image properties.
+
+  var getPokemon = function(pokeId) {
+    var pokey = {};
+    $http.get("http://pokeapi.co/api/v1/pokemon/" + pokeId)
+      .success(function(data) {
+        pokey.abilities = data.abilities;
+        pokey.evolvesTo = data.evolutions[0].to;
+        $http.get("http://pokeapi.co/api/v1/sprite/" + pokeId)
+          .success(function(data) {
+            pokey.name = capitalize(data.pokemon.name);
+            pokey.image = data.image;
+          });
+      })
+    return pokey;
+  };
+
+  // Stubbed out pokemon ids:
+  var pokemonIds = [1,2,3,4,5,6,7,8,9,10,11,56];
+  pokemonIds.forEach(function(pokemonId, ind) {
+    $scope.pokemon[ind] = getPokemon(pokemonId);
+  })
+
+
+
 
 }]);

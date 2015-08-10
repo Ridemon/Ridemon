9353e47919f0cd107c411016e28dc46f7ccc3e75
Thomas Logan <tdloganj@gmail.com>
2015-08-10 14:39:05 -0700

    Leaderboards fixed! Load time greatly decreased.

--- client/components/leaderboard/leaderboard.controller.js
+++ client/components/leaderboard/leaderboard.controller.js
@@ -1,25 +1,4 @@
 RidemonApp.controller('LeaderboardController', ['$scope', '$firebase', '$firebaseArray', '$firebaseObject', "$http", function($scope, $firebase, $firebaseArray, $firebaseObject, $http) {
   var users = new Firebase('https://ridemon.firebaseio.com/users/userIds');
-  $scope.userArray = [];
-
-  users.orderByKey().on("child_added", function(snapshot) {
-    $scope.userArray.unshift(getAllPokemon(snapshot.val(), snapshot.key()));
-  });
-
-  function getAllPokemon(user, userId) {
-    $http.get("/pokedex", {params: {
-      specialUid: userId
-    }}).success(function(data) {
-      user.Uid = userId;
-      user.count = 0;
-      for(var i in user.pokemonIds) {
-        user.count++;
-      }
-      })
-      .error(function(err) {
-        console.error(err);
-      });
-    return user;
-  };
-
+  $scope.userNames = $firebaseArray(users);
 }])

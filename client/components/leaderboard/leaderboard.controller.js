a04e1b64292c1e62b4dcd881409a83ade6904863
Thomas Logan <tdloganj@gmail.com>
2015-08-10 14:41:54 -0700

    removed excess dependency injections.

--- client/components/leaderboard/leaderboard.controller.js
+++ client/components/leaderboard/leaderboard.controller.js
@@ -1,4 +1,4 @@
-RidemonApp.controller('LeaderboardController', ['$scope', '$firebase', '$firebaseArray', '$firebaseObject', "$http", function($scope, $firebase, $firebaseArray, $firebaseObject, $http) {
+RidemonApp.controller('LeaderboardController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
   var users = new Firebase('https://ridemon.firebaseio.com/users/userIds');
   $scope.userNames = $firebaseArray(users);
 }])

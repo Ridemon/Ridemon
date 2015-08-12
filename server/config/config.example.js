c7010e2b09b2ee78b072f11779e5f2007d9f3b8e
travisjn <tdizzletrav@gmail.com>
2015-08-12 11:17:02 -0700

    Remove unnecessary cancel function

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,5 +1,6 @@
 module.exports = {
   clientID: 'CLIENT ID CODE',
   clientSecret: 'CLIENT SECRET TEXT',
-  callbackURL: "http://localhost:3333/auth/uber/callback"
+  callbackURL: "http://localhost:3333/auth/uber/callback",
+  uberURL: 'https://sandbox-api.uber.com/v1/'
 }

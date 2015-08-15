aa15a0c40d57d943ff462e9efe02d64f5c905fa6
travisjn <tdizzletrav@gmail.com>
2015-08-15 10:29:46 -0700

    Complete request controller refactor

--- client/components/request/request.controller.js
+++ client/components/request/request.controller.js
@@ -13,45 +13,10 @@ RidemonApp.controller("RequestController", ["$scope", "$http", "$q", "$sce", "Ri
     $scope.request.start_address = $scope.current.address;
   };
 
-
   // This function gives permission for angular to load map content in an iframe
   $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
   };
-
-  $scope.charityList = [
-    {
-      name: 'St. Anthony Foundation',
-      address: '150 Golden Gate Ave. San Francisco, CA 94102',
-      pokemon: 'MewTwo',
-      pokemonID: 150
-    },
-    {
-      name: 'SF-Marin Food Bank',
-      address: '900 Pennsylvania Ave. San Francisco, CA  94107',
-      pokemon: 'Articuno',
-      pokemonID: 144
-    },
-    {
-      name: 'SF SPCA Pet Adoption Center',
-      address: '250 Florida Street San Francisco CA 94103',
-      pokemon: 'Zapdos',
-      pokemonID: 145
-    },
-    {
-      name: 'HandsOn Bay Area',
-      address: '1504 Bryant Street, San Francisco, CA 94103',
-      pokemon: 'Moltres',
-      pokemonID: 146
-    },
-  ];
-
-  $scope.charityList.forEach(function(charity) {
-    $http.get("http://pokeapi.co/api/v1/sprite/" + (++charity.pokemonID))
-        .success(function(data) {
-          charity.image = data.image;
-      });
-  });
 
   $scope.requestRide = function(charity) {
     $scope.message = "";
@@ -59,12 +24,10 @@ RidemonApp.controller("RequestController", ["$scope", "$http", "$q", "$sce", "Ri
     RideService.rideMaker($scope.request.start_address, $scope.request.end_address, function(rideObject) {
       // After request, set $scope.ride which will render the map view
       $scope.ride = rideObject;
-      console.log($scope.ride.cancelRide);
       // Resize the map to the current view size
       RideService.resizeMap();
     });
   }
-
 
   $scope.cancelRide = function() {
     $scope.ride.cancelRide(function() {
@@ -81,6 +44,42 @@ RidemonApp.controller("RequestController", ["$scope", "$http", "$q", "$sce", "Ri
       $scope.message = address;
       $scope.reset();
     }
-  })
-
+  });
 }]);
+
+  // SHOULD MOVE OUT OF CONTROLLER INTO A CHARITIES FILE - WANT TO MAKE THIS LIST
+  // DYNAMIC BY GETTING CHARITY INFO FROM AN API
+  // $scope.charityList = [
+  //   {
+  //     name: 'St. Anthony Foundation',
+  //     address: '150 Golden Gate Ave. San Francisco, CA 94102',
+  //     pokemon: 'MewTwo',
+  //     pokemonID: 150
+  //   },
+  //   {
+  //     name: 'SF-Marin Food Bank',
+  //     address: '900 Pennsylvania Ave. San Francisco, CA  94107',
+  //     pokemon: 'Articuno',
+  //     pokemonID: 144
+  //   },
+  //   {
+  //     name: 'SF SPCA Pet Adoption Center',
+  //     address: '250 Florida Street San Francisco CA 94103',
+  //     pokemon: 'Zapdos',
+  //     pokemonID: 145
+  //   },
+  //   {
+  //     name: 'HandsOn Bay Area',
+  //     address: '1504 Bryant Street, San Francisco, CA 94103',
+  //     pokemon: 'Moltres',
+  //     pokemonID: 146
+  //   },
+  // ];
+
+  // $scope.charityList.forEach(function(charity) {
+  //   $http.get("http://pokeapi.co/api/v1/sprite/" + (++charity.pokemonID))
+  //       .success(function(data) {
+  //         charity.image = data.image;
+  //     });
+  // });
+
